d0e81685f3bd324c365f763c2cbcf1693d850b74
Ricky Miller <ricky.miller@gmail.com>
2021-10-13 14:03:58 -0400

    try --runInBand

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ process.env.TZ = 'America/Toronto';
 const config = {
 	preset: 'react-native',
 	setupFiles: ['<rootDir>/app/util/testSetup.js'],
-	globalTeardown: '<rootDir>/app/util/testTeardown.js',
+	// globalTeardown: '<rootDir>/app/util/testTeardown.js',
 	transform: {
 		'^.+\\.js$': '<rootDir>jest.preprocessor.js',
 	},
