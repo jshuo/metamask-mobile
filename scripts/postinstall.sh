b0477808f2db399b97a99ae42f68b8fdc9ca5841
Jeff Shuo <jeffshuo@gmail.com>
2021-10-26 20:29:30 -0700

    removed ios

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -10,10 +10,4 @@ npx jetify
 echo "3. Patch npm packages"
 npx patch-package
 
-echo "4. Create xcconfig files..."
-echo "" > ios/debug.xcconfig
-echo "" > ios/release.xcconfig
 
-echo "5. Init git submodules"
-echo "This may take a while..."
-git submodule update --init
